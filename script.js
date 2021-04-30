6c2d42603a7076f37c7854b3f024055e4ca58faa
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-05-01 03:02:04 +0530

    doc update

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
+//DOM references for manipulation later
 const container = document.querySelector(".container");
 const hourEl = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const secondEl = document.querySelector(".second");
 const normalEl = document.querySelector(".normal");
 
-container;
-
+//Setup marking for hour count and minute heads
 setupHourMarkings();
 setupMinuteMarkings();
 
+//Run a update function every second to update the counter
 setInterval(() => {
     setupClockTime();
 }, 1000);
 
+/**
+ * @author Jeeva Kalaiselvam
+ * This function will get the current time and update the UI to set the hours, minutes and seconds based on the data
+ */
 function setupClockTime() {
+    //Getting current hour, minute and second
     let day = new Date();
     let hour = day.getHours();
     let minute = day.getMinutes();
     let second = day.getSeconds();
     let meridian = "AM";
 
+    //Conversion of 24 hour time into 12 hour time
     if (hour > 12) {
         meridian = "PM";
         hour = hour - 12;
@@ -27,11 +34,16 @@ function setupClockTime() {
 
     normalEl.innerHTML = `${hour}:${minute} ${meridian}`;
 
+    //Based on current hour, minute and second, Rotate the element from origin to simulate movement of clock hands
     hourEl.style.transform = `rotate(${hour * 30}deg)`;
     minuteEl.style.transform = `rotate(${minute * 6}deg)`;
     secondEl.style.transform = `rotate(${second * 6}deg)`;
 }
 
+/**
+ * @author Jeeva Kalaiselvam
+ * This function will generate all hour markings
+ */
 function setupHourMarkings() {
     for (let index = 1; index <= 12; index++) {
         const hourMarking = document.createElement("div");
@@ -48,6 +60,10 @@ function setupHourMarkings() {
     }
 }
 
+/**
+ * @author Jeeva Kalaiselvam
+ * This function will generate all minute markings
+ */
 function setupMinuteMarkings() {
     for (let index = 1; index <= 60; index++) {
         const hourMarking = document.createElement("div");
